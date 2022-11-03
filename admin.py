c93859c61edfd8bf2acfa6297fe53f358f6a1c51
StellaAmy <114594039+StellaAmy@users.noreply.github.com>
2022-11-03 21:34:35 +0100

    Add files via upload

--- admin.py
+++ admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Song)
+admin.site.register(Artiste)
+admin.site.register(Lyric)
--- views.py
+++ views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return Httpresponse('welcome to our site')
